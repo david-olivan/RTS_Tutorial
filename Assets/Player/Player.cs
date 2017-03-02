81d265d583bdea657f857bf1a67e24c3b903a98e
david-olivan <david.olivan.93@gmail.com>
2017-03-02 22:58:00 +0100

    First two tutorials

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string username;
+	public bool human;
+
+	private void Start () {
+	}
+
+	private void Update () {
+	}
+}
